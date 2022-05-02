035ce79bd7b680bf9b312e40c84b5d87f499e1d4
Zel <jules.hermyer@gmail.com>
2022-05-02 11:02:29 +0200

    début d'une fonction mvt

--- Biham_Middleton-Levine/biham.ml
+++ Biham_Middleton-Levine/biham.ml
@@ -36,18 +36,37 @@ let draw_tableau l =
 				done
 done
 
-  
+
+let mvt l = 
+	let xmax = Array.length l.(0) in
+	let ymax = Array.length l in
+	for y = 0 to ymax - 1 do	 
+  		for x = 0 to xmax - 1 do
+			match l.(y).(x) with 
+			|Empty -> ()
+			|Red -> if x+1 = xmax then
+				if l.(y).(0) != Empty then ()
+						    else begin l.(y).(0) <- Red; l.(y).(x) <- Empty end
+				
+				else
+				if l.(y).(x+1) <> Empty then ()
+				else begin l.(y).(x+1) <- Red;l.(y).(x) <- Empty end
+			|Blue -> ()
+		
+done
+done
   let _ = 
 	let grille = Array.make_matrix (1280/2) (720/2)  Empty in
 	open_graph " 1280x720";
 	remember_mode true;
 	display_mode false;
         fill_grille 10 grille;
-        let sleep = ref 0.5 in
+        let sleep = ref 0.05 in
         for _ = 1 to 90000  do   
                                 clear_graph();
                                 draw_tableau grille; 
                                 synchronize();
+				mvt grille;
                                 if key_pressed () then let c = read_key () in
                                 match c with 
                                 |'c' -> sleep:= !sleep +. 0.05
