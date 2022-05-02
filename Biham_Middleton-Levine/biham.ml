df0d05a698093d604b4c7079eee067c311e0e133
Zel <jules.hermyer@gmail.com>
2022-05-02 11:33:07 +0200

    à la place de reparcourir 2 fois le tableau à chaque fois on trace les cases en même temps qu'on parcourt pour le mouvement, la vitesse devrait être doublée en théorie si les cases s'affichaient ?

--- Biham_Middleton-Levine/biham.ml
+++ Biham_Middleton-Levine/biham.ml
@@ -36,6 +36,21 @@ let draw_tableau l =
 				done
 done
 
+let draw_case x y l =
+let xmax = Array.length l.(0) in
+let ymax = Array.length l in	
+let xstep = 1280 / xmax in 
+let ystep = 1280 / ymax in	
+match l.(y).(x) with
+|Empty -> set_color (rgb 255 255 255); 
+	fill_rect (x*xstep) (y*ystep) (xstep) (ystep)
+
+|Red -> set_color (rgb 219 48 105); 
+	fill_rect (x*xstep) (y*ystep) (xstep) (ystep)
+
+|Blue ->  set_color (rgb 20 70 160); 
+	fill_rect (x*xstep) (y*ystep) (xstep) (ystep)
+
 
 let mvt l = 
 	let xmax = Array.length l.(0) in
@@ -46,14 +61,16 @@ let mvt l =
 			|Empty -> ()
 			|Red -> if x+1 = xmax then
 				if l.(y).(0) != Empty then ()
-						    else begin l.(y).(0) <- Red; l.(y).(x) <- Empty end
+						    else begin l.(y).(0) <- Red; draw_case 0 y l;
+							l.(y).(x) <- Empty ; draw_case x y l end
 				
 				else
 				if l.(y).(x+1) <> Empty then ()
 				else begin l.(y).(x+1) <- Red;l.(y).(x) <- Empty end
 			|Blue -> if y+1 = ymax then
 				if l.(0).(x) != Empty then ()
-						    else begin l.(0).(x) <- Blue; l.(y).(x) <- Empty end
+						    else begin l.(0).(x) <- Blue; draw_case x 0 l
+							; l.(y).(x) <- Empty; draw_case x y l end
 				
 				else
 				if l.(y+1).(x) <> Empty then ()
@@ -67,14 +84,14 @@ done
 	remember_mode true;
 	display_mode false;
         fill_grille 10 grille;
+        draw_tableau grille;	
         let sleep = ref 0.00005 in
         for _ = 1 to 90000  do   
-                                clear_graph();
-                                draw_tableau grille; 
                                 synchronize();
 				mvt grille;
                                 if key_pressed () then let c = read_key () in
-                                match c with 
+                                match c with
+				|'r' -> draw_tableau grille 
                                 |'c' -> sleep:= !sleep +. 0.05
                                 |'b' -> sleep:= !sleep -. 0.05
                                 |_ -> () 
