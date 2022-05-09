4f7b15d55ff5345941758ea4ca940597fc3e7277
Zel <jules.hermyer@gmail.com>
2022-05-09 11:00:28 +0200

    debut d'une transition vers raylib, expérimental

--- Biham_Middleton-Levine/biham.ml
+++ Biham_Middleton-Levine/biham.ml
@@ -1,4 +1,3 @@
-open Graphics
 type case = Empty | Blue | Red
 
 
@@ -21,24 +20,29 @@ let draw_case x y l =
 let xmax = Array.length l.(0) in
 let ymax = Array.length l in	
 let xstep = 1280 / xmax in 
-let ystep = 1280 / ymax in	
+let ystep = 1280 / ymax in
+let open Raylib in
+let emptycolor = Color.create 255 255 255 255 in
+let redcolor = Color.create 219 48 105 255 in
+let bluecolor = Color.create 20 70 160 255 in 	
 match l.(y).(x) with
-|Empty -> set_color (rgb 255 255 255); 
-	fill_rect (x*xstep) (y*ystep) (xstep) (ystep)
-
-|Red -> set_color (rgb 219 48 105); 
-	fill_rect (x*xstep) (y*ystep) (xstep) (ystep)
-
-|Blue ->  set_color (rgb 20 70 160); 
-	fill_rect (x*xstep) (y*ystep) (xstep) (ystep)
-
-
+|Empty ->begin begin_drawing ();
+	draw_rectangle (x*xstep) (y*ystep) (xstep) (ystep) emptycolor;
+	end_drawing();
+	end
+|Red -> begin begin_drawing ();
+	draw_rectangle (x*xstep) (y*ystep) (xstep) (ystep) redcolor;
+	end_drawing();
+	end                                        
+|Blue -> begin begin_drawing ();
+	draw_rectangle (x*xstep) (y*ystep) (xstep) (ystep) bluecolor;
+	end_drawing();
+	end
+     
 let draw_tableau l =
 
 	let xmax = Array.length l.(0) in
 	let ymax = Array.length l in
-	set_color (rgb 31 19 0);
-        fill_rect 0 0 1280 720;
 	for y = 0 to ymax-1 do
 		for x = 0 to xmax-1  do
 		draw_case x y l
@@ -72,23 +76,30 @@ let mvt l =
 		
 done
 done
-  let _ = 
-	let grille = Array.make_matrix (1280/2) (720/2)  Empty in
-	open_graph " 1280x720";
-	remember_mode true;
-	display_mode false;
-        fill_grille 10 grille;
-        draw_tableau grille;	
-        let sleep = ref 0.00005 in
-        for _ = 1 to 90000  do  
-				mvt grille;
-                                synchronize();
-                                if key_pressed () then let c = read_key () in
-                                match c with
-				|'r' -> draw_tableau grille 
-                                |'c' -> sleep:= !sleep +. 0.05
-                                |'b' -> sleep:= !sleep -. 0.05
-                                |_ -> () 
-                                else
-                                Unix.sleepf !sleep;
-        done
+
+
+let setup () = 
+	Raylib.init_window 1280 720 "biham model";
+	Raylib.set_target_fps 144
+
+
+let rec loop grille =
+	match Raylib.window_should_close () with
+		|true -> Raylib.close_window ()
+		|false ->
+	let open Raylib in 
+	begin_drawing ();
+	mvt grille;
+	draw_tableau grille;
+	end_drawing();
+	loop grille
+
+let _ =
+let grille = Array.make_matrix (1280/2) (720/2) Empty in 
+fill_grille 10 grille;
+setup (); 
+loop grille;
+
+
+
+
